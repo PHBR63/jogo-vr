77f6b61e0db3a7b45484a95bc083af6fdcf9573b
Pedro Henrique Ferreira Zampronha <phzf123@gmail.com>
2020-06-24 20:14:42 -0300

    final

---
+++ Assets/scripts/CountdownTimer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public string leveltoload;
+    private float timer = 180f;
+    private Text timerSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        
+        timerSeconds.text = timer.ToString("f2");
+
+        if (timer <= 0)
+        {
+            Application.LoadLevel(leveltoload);
+        }
+    }
+}
---
+++ Assets/scripts/DestroyOnTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    public GameObject objToDestroy;
+    public GameObject effect;
+    public string leveltoload;
+    private bool counter = false;
+    private float timer = 7f;
+   
+
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") { 
+
+            Instantiate(effect, objToDestroy.transform.position, objToDestroy.transform.rotation);
+
+            Destroy(objToDestroy, 6.5F);
+            counter = true;
+        }
+
+        
+    }
+
+    void Update()
+    {
+        
+
+        if (counter == true)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+            {
+                Application.LoadLevel(leveltoload);
+            }
+            
+        }
+
+        
+    }
+
+}
